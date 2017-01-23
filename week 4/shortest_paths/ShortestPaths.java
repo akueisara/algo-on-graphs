6f1897043665aef64f44513e6db94621fb5d8f40
Kuei-Jung Hu <akueisara39@gmail.com>
2017-01-23 16:35:43 -0600

    Update ShortestPaths.java

--- week 4/shortest_paths/ShortestPaths.java
+++ week 4/shortest_paths/ShortestPaths.java
@@ -2,69 +2,52 @@ import java.util.*;
 
 public class ShortestPaths {
 
-    private static void shortestPaths(ArrayList<Integer>[] adj, ArrayList<Integer>[] cost, int s, long[] distance, int[] reachable, int[] shortest) {
-      //write your code here
-	  distance[s] = 0;
-	  reachable[s] = 1;
-	  Queue<Integer> queue = new LinkedList<>();
-	  for (int i = 0; i < adj.length; i++) {
-        for (int u = 0; u < adj.length; u++) {
-          for (int v : adj[u]) {
-			int v_index = adj[u].indexOf(v);
-			if (distance[u] != Long.MAX_VALUE && distance[v] > distance[u] + cost[u].get(v_index)) {
-			  distance[v] = distance[u] + cost[u].get(v_index);
-			  reachable[v] = 1;
-			  if(i == adj.length - 1) {
-                queue.add(v);
-			  }
-			}
-		  }
-		}
-      }
-
-	  int[] visited = new int[adj.length];
-	  while (!queue.isEmpty()) {
-		int u = queue.remove();
-        visited[u] = 1;
-	    if(u != s)
-	      shortest[u] = 0;
-        for (int v : adj[u]) {
-		  if (visited[v] == 0) {
-            queue.add(v);
-			visited[v] = 1;
-			shortest[v] = 0;
-		  }
+    private static void shortestPaths(ArrayList < Integer > [] adj, ArrayList < Integer > [] cost, int s, long[] distance, int[] reachable, int[] shortest) {
+        //write your code here
+        distance[s] = 0;
+        reachable[s] = 1;
+        Queue < Integer > queue = new LinkedList < > ();
+        for (int i = 0; i < adj.length; i++) {
+            for (int u = 0; u < adj.length; u++) {
+                for (int v: adj[u]) {
+                    int v_index = adj[u].indexOf(v);
+                    if (distance[u] != Long.MAX_VALUE && distance[v] > distance[u] + cost[u].get(v_index)) {
+                        distance[v] = distance[u] + cost[u].get(v_index);
+                        reachable[v] = 1;
+                        if (i == adj.length - 1) {
+                            queue.add(v);
+                        }
+                    }
+                }
+            }
         }
-	  }
-	  distance[s] = 0;
+
+        int[] visited = new int[adj.length];
+        while (!queue.isEmpty()) {
+            int u = queue.remove();
+            visited[u] = 1;
+            if (u != s)
+                shortest[u] = 0;
+            for (int v: adj[u]) {
+                if (visited[v] == 0) {
+                    queue.add(v);
+                    visited[v] = 1;
+                    shortest[v] = 0;
+                }
+            }
+        }
+        distance[s] = 0;
     }
-	
-	private static Set<Integer> bfs(ArrayList<Integer>[] adj, Queue<Integer> queue) {
-	  Set<Integer> result = new HashSet<Integer>();
-      int[] visited = new int[adj.length];
-	  while (!queue.isEmpty()) {
-		int u = queue.remove();
-		result.add(u);
-		visited[u] = 1;
-        for (int v : adj[u]) {
-		  if (visited[v] == 0) {
-            queue.add(v);
-		  }
-        }
-	  }
-	  return result;
-	}
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        ArrayList<Integer>[] cost = (ArrayList<Integer>[])new ArrayList[n];
+        ArrayList < Integer > [] adj = (ArrayList < Integer > []) new ArrayList[n];
+        ArrayList < Integer > [] cost = (ArrayList < Integer > []) new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
-            cost[i] = new ArrayList<Integer>();
+            adj[i] = new ArrayList < Integer > ();
+            cost[i] = new ArrayList < Integer > ();
         }
         for (int i = 0; i < m; i++) {
             int x, y, w;
@@ -96,4 +79,3 @@ public class ShortestPaths {
     }
 
 }
-
